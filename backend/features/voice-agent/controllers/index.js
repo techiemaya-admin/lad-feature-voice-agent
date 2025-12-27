73bf079a4f7a9b63385b1f234e93929586140568
Suhas7981 <suhas.c@techiemaya.com>
2025-12-27 12:08:40 +0530

    added two files

--- backend/features/voice-agent/controllers/index.js
+++ backend/features/voice-agent/controllers/index.js
@@ -6,8 +6,12 @@
 
 const VoiceAgentController = require('./VoiceAgentController');
 const CallController = require('./CallController');
+const BatchCallController = require('./call-controllers/BatchCallController');
+const CallInitiationController = require('./call-controllers/CallInitiationController');
 
 module.exports = {
   VoiceAgentController,
-  CallController
+  CallController,
+  BatchCallController,
+  CallInitiationController
 };
